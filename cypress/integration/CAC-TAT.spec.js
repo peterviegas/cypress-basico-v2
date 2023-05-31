c6e94afe8f7a949e2765b294dd4620d24cc1957d
Peter Viegas <peterviegas@outlook.com>
2023-05-31 20:33:34 +0100

    mark the type of service Feedback

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -100,9 +100,15 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .should('have.value', 'mentoria')
     })
 
-    it.only('seleciona um produto (Blog) por seu índice', function() {
+    it('seleciona um produto (Blog) por seu índice', function() {
         cy.get('#product')
             .select(1)
             .should('have.value', 'blog')
+    })
+
+    it.only('marca o tipo de atendimento Feedback', function() {
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value', 'feedback')
     })
 })
