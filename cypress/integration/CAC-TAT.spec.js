5babf488be5fa4c019628f32803f746e914adbc8
Peter Viegas <peterviegas@outlook.com>
2023-05-31 07:14:22 +0100

    move the visit to the test application page to a beforeEach() block

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,17 @@
+// CAC-TAT.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+describe('Central de Atendimento ao Cliente TAT', function() {
+    beforeEach(function() {
+        cy.visit('./src/index.html')
+    })
+    it('verifica o título da aplicação', function() {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+
+})
