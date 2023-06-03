88e6f21e81664ce33ede01ab5fa9b39243a06bc6
Peter Viegas <peterviegas@outlook.com>
2023-06-03 21:11:54 +0100

    make an HTTP request

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -209,11 +209,22 @@ describe('Central de Atendimento ao Cliente TAT', function() {
             .should('not.be.visible')
     })
 
-    it.only('preenche a area de texto usando o comando invoke', () => {
+    it('preenche a area de texto usando o comando invoke', () => {
         const longText = Cypress._.repeat('0123456789', 20)
         cy.get('#open-text-area')
             .invoke('val', longText)
             .should('have.value', longText)
 
+    })
+
+    it.only('faz uma requisição HTTP', () => {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+            .should(function(response) {
+                console.log(response)
+                const { status, statusText, body } = response
+                expect(status).to.equal(200)
+                expect(statusText).to.equal('OK')
+                expect(body).to.include('CAC TAT')
+            })
     })
 })
